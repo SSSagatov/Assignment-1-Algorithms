7a6252a318b81f9d9fe19483cd5c56bcdd57c8cf
ssagatov <you@example.com>
2025-09-27 08:08:42 -0700

    Added full function of merge. Merge sort done without quick sort for short arrays

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -1,25 +1,52 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MergeSort {
 
+    // merge 2 subarrays of array
     void merge(int[] arr, int left, int mid, int right) {
+        // calculate the left and right sides
         int n1 = mid - left + 1;
         int n2 = right - mid;
 
+        // send it to 2 arrays(left and right)
         int[] L =  new int[n1];
         int[] R = new int[n2];
 
-        for (int i = 0; i < n1; i++) {
+        //copy elements from array to L(left) and R(right)
+        for (int i = 0; i < n1; i++)
             L[i] = arr[left+i];
-        }
 
-        for (int j = 0; j < n2; j++) {
+        for (int j = 0; j < n2; j++)
             R[j] =  arr[mid+1+j];
+
+        // Merge the temp arrays
+        // Initial indexes of both subarrays
+        int i = 0, j = 0;
+        int k = left;
+
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
         }
 
-        int i = 0, j = 0;
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
 
-        int k = left;
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
 
     }
 
@@ -27,17 +54,33 @@ public class MergeSort {
         if (left < right) {
             int mid = (left + right) / 2;
 
+            //sort both side by calling the same function(recursion)
             sort(array, left, mid);
             sort(array,mid + 1, right);
 
+            //merge the final result
             merge(array, left, mid, right);
         }
     }
 
     public static void main(String[] args) {
-        int[] arr = {4,3,6,4,7,8,9,4,5,3,2};
+        //testing array
+//      int[] arr = {4,3,6,4,7,8,9,4,5,3,2};
+        //Input the length of array
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
 
+        //new array to store values
+        int[] arr = new int[n];
+        //loop to send values into array
+        for  (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Calling the merge function
+        // Also send the args to sort function
         MergeSort ms = new MergeSort();
         ms.sort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
     }
 }
